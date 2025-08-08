cdd94249aec1b923a5cb39eedc7be283872f62b2
Iago Anjos <iagokaued.com>
2025-08-08 20:08:38 -0300

    feat: adiciona funcionalidades de filtro (Todas, Pendentes, Concluídas)

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import "../styles/TaskItem.css";
+import "../styles/TodoList.css";
+import TaskItem from "./TaskItem";
 
 interface Task {
   id: number;
@@ -7,10 +9,14 @@ interface Task {
   completed: boolean;
 }
 
+type FilterType = "all" | "completed" | "pending";
+
 export function TodoList() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   const [input, setInput] = useState<string>("");
+
+  const [filter, setFilter] = useState<FilterType>("all");
 
   useEffect(() => {
     const savedTasks = localStorage.getItem("tasks");
@@ -38,6 +44,34 @@ export function TodoList() {
     setInput("");
   };
 
+  const handleDeleteTask = (id: number) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const handleToggleTask = (id: number) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const handleEditTask = (id: number, newText: string) => {
+    setTasks(
+      tasks.map((task) => (task.id === id ? { ...task, text: newText } : task))
+    );
+  };
+
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "completed") {
+      return task.completed;
+    }
+    if (filter === "pending") {
+      return !task.completed;
+    }
+    return true; // Retorna todas as tarefas se o filtro for 'all'
+  });
+
   return (
     <div className="todo-list-container">
       <h1>Lista de Tarefas</h1>
@@ -50,9 +84,37 @@ export function TodoList() {
         />
         <button type="submit">Adicionar</button>
       </form>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>{task.text}</li>
+
+      <div className="filter-buttons">
+        <button
+          onClick={() => setFilter("all")}
+          className={filter === "all" ? "active" : ""}
+        >
+          Todas
+        </button>
+        <button
+          onClick={() => setFilter("pending")}
+          className={filter === "pending" ? "active" : ""}
+        >
+          Pendentes
+        </button>
+        <button
+          onClick={() => setFilter("completed")}
+          className={filter === "completed" ? "active" : ""}
+        >
+          Concluídas
+        </button>
+      </div>
+      <ul className="task-list">
+        {/* Renderiza a lista de tarefas filtrada */}
+        {filteredTasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            onDelete={handleDeleteTask}
+            onToggle={handleToggleTask}
+            onEdit={handleEditTask}
+          />
         ))}
       </ul>
     </div>
